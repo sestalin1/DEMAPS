29907994cf8ecab5bba9e337a46e6c4cab3688f5
sestalin <stalin24-7@hotmail.com>
2016-12-09 23:10:27 -0400

    Version inicial

---
+++ DemapAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemapAdmin.Startup))]
+namespace DemapAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
